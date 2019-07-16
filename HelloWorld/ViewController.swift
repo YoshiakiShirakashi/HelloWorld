1d0bfbd1c4469ff08ccd05965c89b679b0622155
YoshiakiShirakashi <mikan014140@yahoo.co.jp>
2019-07-16 19:41:33 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 白樫芳昭 on 2019/07/16.
+//  Copyright © 2019 yoshiaki.sjirakashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
